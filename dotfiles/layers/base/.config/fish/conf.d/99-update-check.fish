63f156eacafb04c24c53e5384a1d49379d6347fc
Beth Rennie <beth@brennie.ca>
2025-08-13 01:34:08 -0400

    fish: add a warning if flake.lock has not been updated in over a week

---
+++ dotfiles/layers/base/.config/fish/conf.d/99-update-check.fish
@@ -0,0 +1,11 @@
+# Print a warning if flake.lock has not been updated in over a week.
+
+set _SCRIPT_PATH (realpath ~/.config/fish/conf.d/99-update-check.fish)
+set _CONFIG_DIR (realpath (dirname $_SCRIPT_PATH)/../../../../../..)
+set _LAST_UPDATE (stat -f %m $_CONFIG_DIR/nix/flake.lock)
+set _NOW (date +%s)
+set _DELTA (math $_NOW - $_LAST_UPDATE)
+
+if [ $_DELTA -gt (math '24 * 60 * 60') ]
+    echo "⚠️ Update Required: flake.lock is over one week old️"
+end
