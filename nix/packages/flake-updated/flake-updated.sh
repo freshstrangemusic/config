20a0507bcc0dd73a994f5403a5c08e1b431f4b60
Beth Rennie <beth@brennie.ca>
2025-08-22 14:09:24 -0400

    nix: Fix flake-updated week duration
    It was accidentally a day instead of a week. Oops.

--- nix/packages/flake-updated/flake-updated.sh
+++ nix/packages/flake-updated/flake-updated.sh
@@ -7,6 +7,6 @@ LOCK_PATH="$1"
 NIXPKGS_LAST_MODIFIED=$(jq < "$LOCK_PATH" .nodes.nixpkgs.locked.lastModified)
 NOW=$(date +%s)
 DELTA=$((NOW - NIXPKGS_LAST_MODIFIED))
-ONE_WEEK=$((24 * 60 * 60))
+ONE_WEEK=$((7 * 24 * 60 * 60))
 
 exit $((DELTA >= ONE_WEEK))
