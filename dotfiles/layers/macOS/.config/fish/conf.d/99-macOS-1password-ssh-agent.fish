49d82491ff6c3b096c540e433107ad282961dcc4
Beth Rennie <beth@brennie.ca>
2026-06-08 09:57:13 -0400

    macOS: set SSH_AUTH_SOCK without path expansion
    On macOS we were setting the SSH agent in two places: in ssh.conf.d/ and
    in a Fish env var. We must set the env var because it overrides the
    configuration in ssh.conf and there is a default value pointing to the
    native macOS ssh-agent. However, including a ~ in the variable results
    in ssh no longer being able to read the path. We now expand the path in
    the file ourselves.

--- dotfiles/layers/macOS/.config/fish/conf.d/99-macOS-1password-ssh-agent.fish
+++ dotfiles/layers/macOS/.config/fish/conf.d/99-macOS-1password-ssh-agent.fish
@@ -1,1 +1,1 @@
-set -gx SSH_AUTH_SOCK "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+set -gx SSH_AUTH_SOCK "/Users/beth/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
